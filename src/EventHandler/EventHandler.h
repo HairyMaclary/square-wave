658f2f0074b26e15a1f6901729387d09c98ec185
HairyMaclary <cliff.hamilton@outlook.com>
2021-07-03 09:31:57 +1200

    Move event handling into separate class

---
+++ src/EventHandler/EventHandler.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "../Setup/Setup.h"
+
+class EventHandler
+{
+private:
+	sf::RenderWindow& window;
+	sf::View& view;
+
+public:
+	EventHandler(sf::RenderWindow& window, sf::View& view) :
+		window { window },
+		view { view }
+	{
+	}
+
+	void process(sf::Event& event)
+	{
+		switch (event.type)
+		{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::Resized:
+				std::cout << "New window width: " << event.size.width << " new height: " << event.size.height << std::endl;
+				resizedView(window, view);
+				break;
+			case sf::Event::TextEntered:
+				printf("%c", event.text.unicode);
+				break;
+			case sf::Event::KeyPressed:
+				if (event.key.code == sf::Keyboard::Key::A)
+				{
+					// player.move(-1.00f, 0.0f);
+				}
+				if (event.key.code == sf::Keyboard::Key::D)
+				{
+					// player.move(1.00f, 0.0f);
+				}
+				break;
+			case sf::Event::GainedFocus:
+			case sf::Event::KeyReleased:
+			case sf::Event::MouseWheelMoved:
+			case sf::Event::MouseWheelScrolled:
+			case sf::Event::MouseButtonPressed:
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					// player.setPosition(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y));
+				}
+				break;
+			case sf::Event::MouseButtonReleased:
+			case sf::Event::MouseMoved:
+			case sf::Event::MouseEntered:
+			case sf::Event::MouseLeft:
+			case sf::Event::JoystickButtonPressed:
+			case sf::Event::JoystickButtonReleased:
+			case sf::Event::JoystickMoved:
+			case sf::Event::JoystickConnected:
+			case sf::Event::JoystickDisconnected:
+			case sf::Event::TouchBegan:
+			case sf::Event::TouchMoved:
+			case sf::Event::TouchEnded:
+			case sf::Event::SensorChanged:
+			case sf::Event::Count:
+			case sf::Event::LostFocus:
+			default:
+				break;
+		}
+	}
+};
+
+// move an object much faster: use the existing object and not an event
+
+// if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+// {
+// 	player.move(-1.00f, 0.0f);
+// }
+
+// if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+// {
+// 	player.move(1.00f, 0.0f);
+// }
+
+// if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+// {
+// 	sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+// 	int mouseX = static_cast<float>(mousePosition.x);
+// 	int mouseY = static_cast<float>(mousePosition.y);
+// 	player.setPosition(mouseX, mouseY);
+// }
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,4 @@
+#include "./EventHandler/EventHandler.h"
 #include "./Setup/Setup.h" // initial window dimensions and generateSetup()
 #include "./Ship/Ship.h"
 #include <iostream>
@@ -14,6 +15,7 @@ main()
 
 	// sf::Clock clock;
 	sf::Event event;
+	EventHandler handler(window, view);
 
 	while (window.isOpen())
 	{
@@ -22,77 +24,8 @@ main()
 
 		while (window.pollEvent(event))
 		{
-			switch (event.type)
-			{
-				case sf::Event::Closed:
-					window.close();
-					break;
-				case sf::Event::Resized:
-					std::cout << "New window width: " << event.size.width << " new height: " << event.size.height << std::endl;
-					resizedView(window, view);
-					break;
-				case sf::Event::TextEntered:
-					printf("%c", event.text.unicode);
-					break;
-				case sf::Event::KeyPressed:
-					if (event.key.code == sf::Keyboard::Key::A)
-					{
-						// player.move(-1.00f, 0.0f);
-					}
-					if (event.key.code == sf::Keyboard::Key::D)
-					{
-						// player.move(1.00f, 0.0f);
-					}
-					break;
-				case sf::Event::GainedFocus:
-				case sf::Event::KeyReleased:
-				case sf::Event::MouseWheelMoved:
-				case sf::Event::MouseWheelScrolled:
-				case sf::Event::MouseButtonPressed:
-					if (event.mouseButton.button == sf::Mouse::Left)
-					{
-						// player.setPosition(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y));
-					}
-					break;
-				case sf::Event::MouseButtonReleased:
-				case sf::Event::MouseMoved:
-				case sf::Event::MouseEntered:
-				case sf::Event::MouseLeft:
-				case sf::Event::JoystickButtonPressed:
-				case sf::Event::JoystickButtonReleased:
-				case sf::Event::JoystickMoved:
-				case sf::Event::JoystickConnected:
-				case sf::Event::JoystickDisconnected:
-				case sf::Event::TouchBegan:
-				case sf::Event::TouchMoved:
-				case sf::Event::TouchEnded:
-				case sf::Event::SensorChanged:
-				case sf::Event::Count:
-				case sf::Event::LostFocus:
-				default:
-					break;
-			}
+			handler.process(event);
 		}
-
-		// move an object much faster: use the existing object and not an event
-
-		// if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-		// {
-		// 	player.move(-1.00f, 0.0f);
-		// }
-
-		// if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-		// {
-		// 	player.move(1.00f, 0.0f);
-		// }
-
-		// if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
-		// {
-		// 	sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-		// 	int mouseX = static_cast<float>(mousePosition.x);
-		// 	int mouseY = static_cast<float>(mousePosition.y);
-		// 	player.setPosition(mouseX, mouseY);
-		// }
 
 		window.clear(sf::Color(0, 0, 0));
 		ship.draw();
