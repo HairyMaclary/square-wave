0a5a6744e835edbed632cde5a18e5e7c336f3892
HairyMaclary <cliff.hamilton@outlook.com>
2021-07-03 13:23:07 +1200

    move direct key interception from keyboard objects into own file for future use

--- src/EventHandler/EventHandler.h
+++ src/EventHandler/EventHandler.h
@@ -13,23 +13,3 @@ public:
 
 	void process(sf::Event& event);
 };
-
-// move an object much faster: use the existing object and not an event
-
-// if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-// {
-// 	player.move(-1.00f, 0.0f);
-// }
-
-// if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-// {
-// 	player.move(1.00f, 0.0f);
-// }
-
-// if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
-// {
-// 	sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-// 	int mouseX = static_cast<float>(mousePosition.x);
-// 	int mouseY = static_cast<float>(mousePosition.y);
-// 	player.setPosition(mouseX, mouseY);
-// }
