e0b36d7d84865a643814d20a8c3311f12986adf0
HairyMaclary <cliff.hamilton@outlook.com>
2021-08-08 12:40:28 +1200

    Move waves to cpp file

---
+++ src/Wave/Wave.cpp
@@ -0,0 +1,37 @@
+#include "Wave.hpp"
+
+void Wave::shiftRight()
+{
+	std::vector<sf::Vertex>::iterator it;
+	for (it = verticies.begin(); it != verticies.end(); ++it)
+	{
+		(*it).position.x += 1;
+	}
+}
+
+void Wave::removeOldValues()
+{
+	while (verticies.size() > window::width)
+	{
+		verticies.pop_back();
+	}
+}
+
+void Wave::addValue(float yValue)
+{
+	shiftRight();
+	sf::Vertex vertex;
+	vertex.position = sf::Vector2f(0.f, yValue);
+	vertex.color = sf::Color::White;
+	verticies.push_back(vertex);
+}
+
+void Wave::update()
+{
+	removeOldValues();
+}
+
+void Wave::draw()
+{
+	window.draw(&verticies[0], verticies.size(), sf::Points);
+}
--- src/Wave/Wave.hpp
+++ src/Wave/Wave.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "Setup/Setup.h"
 #include <list> // std::list
 
@@ -9,46 +11,17 @@ private:
 	sf::RenderWindow& window;
 
 	// Each vertex is one pixel. As a new vertex is added we need to shift right
-	void shiftRight()
-	{
-		std::vector<sf::Vertex>::iterator it;
-		for (it = verticies.begin(); it != verticies.end(); ++it)
-		{
-			(*it).position.x += 1;
-		}
-	}
+	void shiftRight();
 
 	// free up memory once for those points that are off screen
-	void removeOldValues()
-	{
-		while (verticies.size() > window::width)
-		{
-			verticies.pop_back();
-		}
-	}
+	void removeOldValues();
 
 public:
 	Wave(sf::RenderWindow& window) :
 		window { window }
-	{
-	}
+	{}
 
-	void addValue(float yValue)
-	{
-		shiftRight();
-		sf::Vertex vertex;
-		vertex.position = sf::Vector2f(0.f, yValue);
-		vertex.color = sf::Color::White;
-		verticies.push_back(vertex);
-	}
-
-	void update()
-	{
-		removeOldValues();
-	}
-
-	void draw()
-	{
-		window.draw(&verticies[0], verticies.size(), sf::Points);
-	}
+	void addValue(float yValue);
+	void update();
+	void draw();
 };
